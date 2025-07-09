915e9a19531e9651c42848ed44b52ce4eaa332b3
Samrat Singh <samrat.singh@f1soft.com>
2025-07-09 17:58:42 +0545

    Replacing snackbar with toastr

---
+++ src/app/shared/toastr-msg.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ToastrService} from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastrMsgService {
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  success(message: string) {
+    this.toastr.success(message, "Status", {timeOut: 3000});
+  }
+
+  error(message: string | '') {
+    if(message === ''){
+      this.toastr.error("Something went wrong!!", "Oops", {timeOut: 3000});
+    }
+      this.toastr.error(message, "Oops", {timeOut: 3000});
+  }
+}
